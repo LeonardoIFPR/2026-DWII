f6065a81fd842a1e5e0e5e93bb8d2d510490019a
LeonardoIFPR <20241ctb0100029@estudantes.ifpr.edu.br>
2026-04-06 20:31:41 +0000

    aula08: painel.php — link para módulo CRUD

--- 04_sessoes/painel.php
+++ 04_sessoes/painel.php
@@ -66,6 +66,7 @@ $pagina_atual = "";
         <h3>Painel de controle</h3>
         <p>Este conteudo só é visivel para usuarios autenticados</p>
         <p>Nas proximas aulas este painel tera funcionalidades reais (CRUD)</p>
+        <a href="../05_crud//index.php" class="btn-primario">Gerenciar Projetos</a>
         <div class="svg_perfil">
             <a href="perfil.php" class="link-engrenagem">
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
